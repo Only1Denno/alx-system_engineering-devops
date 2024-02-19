e68b7f4defceaedab746092a093779fb88a937e6
Only1Denno <only1denno@gmail.com>
2024-02-19 02:02:59 -0800

    1. Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"subscribers count"
-
+"""
+Write a function that queries the Reddit API and
+returns the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
+    """ function to check number of subscribers """
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    else:
-        return data['data']['subscribers']
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'chrome:0x16-api_advanced:v1'}
+
+    response = requests.get(url, headers=headers).json()
+    subs = response.get('data', {}).get('subscribers', 0)
+    return subs
